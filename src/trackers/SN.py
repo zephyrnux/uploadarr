fd02bdd54046ac11ce238586598c095bb2333d73
z-ink <159651875+z-ink@users.noreply.github.com>
2024-05-06 20:26:40 -0400

    Update SN.py

--- src/trackers/SN.py
+++ src/trackers/SN.py
@@ -21,7 +21,6 @@ class SN():
         self.tracker = 'SN'
         self.source_flag = 'Swarmazon'
         self.upload_url = 'https://swarmazon.club/api/upload.php'
-        self.forum_link = 'https://swarmazon.club/php/forum.php?forum_page=2-swarmazon-rules'
         self.search_url = 'https://swarmazon.club/api/search.php'
         self.signature = f"\n[center][size=6][url=https://github.com/z-ink/Upload-Assistant]Upload Assistant(CvT Mod v0.3)[/url][/size][/center]"
         self.anon_signature = f"\n[center][size=6]we are anonymous[/size][/center]"        
