67508ec1b059a6d66e5afc693429b0bda871429d
z-ink <159651875+z-ink@users.noreply.github.com>
2024-05-06 20:28:46 -0400

    Update ULCX.py
    anon sig support

--- src/trackers/ULCX.py
+++ src/trackers/ULCX.py
@@ -31,7 +31,8 @@ class ULCX():
         self.source_flag = 'ULCX'
         self.upload_url = 'https://upload.cx/api/torrents/upload'
         self.search_url = 'https://upload.cx/api/torrents/filter'
-        self.signature = f"\n[center][url=https://github.com/z-ink/Upload-Assistant]Created by L4G's Upload Assistant[/url][/center]"
+        self.signature = f"\n[center][size=6][url=https://github.com/z-ink/Upload-Assistant]Upload Assistant(CvT Mod v0.3)[/url][/size][/center]"
+        self.anon_signature = f"\n[center][size=6]we are anonymous[/size][/center]"
         self.banned_groups = ['Tigole', 'x0r', 'Judas', 'SPDVD', 'MeGusta', 'YIFY', 'SWTYBLZ', 'TAoE', 'TSP', 'TSPxL', 'LAMA', '4K4U', 'ION10', 'Will1869', 'TGx', 'Sicario', 'QxR', 'Hi10', 'EMBER', 'FGT', 'AROMA', 'd3g', 'nikt0', 'RARBG']
         pass
     
