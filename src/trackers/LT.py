2658d682367598871fdc058e00abb18533fd57e9
z-ink <159651875+z-ink@users.noreply.github.com>
2024-05-06 20:21:44 -0400

    Update LT.py
    anon sig support

--- src/trackers/LT.py
+++ src/trackers/LT.py
@@ -31,7 +31,8 @@ class LT():
         self.source_flag = 'Lat-Team "Poder Latino"'
         self.upload_url = 'https://lat-team.com/api/torrents/upload'
         self.search_url = 'https://lat-team.com/api/torrents/filter'
-        self.signature = ''
+        self.signature = f"\n[center][size=6][url=https://github.com/z-ink/Upload-Assistant]Upload Assistant(CvT Mod v0.3)[/url][/size][/center]"
+        self.anon_signature = f"\n[center][size=6]we are anonymous[/size][/center]"
         self.banned_groups = [""]
         pass
     
