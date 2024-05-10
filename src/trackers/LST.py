1be3b516a2c55bdc4aa9ec2bd5f703eeeb87aa9f
z-ink <159651875+z-ink@users.noreply.github.com>
2024-05-10 17:08:59 -0400

    Update LST.py
    Removed signatures. Now in config.

--- src/trackers/LST.py
+++ src/trackers/LST.py
@@ -31,8 +31,6 @@ class LST():
         self.source_flag = 'LST.GG'
         self.upload_url = 'https://lst.gg/api/torrents/upload'
         self.search_url = 'https://lst.gg/api/torrents/filter'
-        self.signature = f"\n[center][size=6][url=https://github.com/z-ink/Upload-Assistant]Upload Assistant(CvT Mod v0.3)[/url][/size][/center]"
-        self.anon_signature = f"\n[center][size=6]we are anonymous[/size][/center]"
         self.banned_groups = [""]
         pass
     
@@ -85,7 +83,7 @@ class LST():
         cat_id = await self.get_cat_id(meta['category'], meta.get('keywords', ''), meta.get('service', ''))
         type_id = await self.get_type_id(meta['type'])
         resolution_id = await self.get_res_id(meta['resolution'])
-        await common.unit3d_edit_desc(meta, self.tracker, self.signature)
+        await common.unit3d_edit_desc(meta, self.tracker)
         region_id = await common.unit3d_region_ids(meta.get('region'))
         distributor_id = await common.unit3d_distributor_ids(meta.get('distributor'))
         if meta['anon'] == 0 and bool(distutils.util.strtobool(str(self.config['TRACKERS'][self.tracker].get('anon', "False")))) == False:
