2ca9feab9e37016d0f08d5cecfaec60feb9d500f
z-ink <159651875+z-ink@users.noreply.github.com>
2024-05-15 10:59:56 -0400

    Removes need for distutils

--- src/trackers/LT.py
+++ src/trackers/LT.py
@@ -2,7 +2,6 @@
 # import discord
 import asyncio
 import requests
-import distutils.util
 import os
 import platform
 
@@ -87,10 +86,10 @@ class LT():
         region_id = await common.unit3d_region_ids(meta.get('region'))
         distributor_id = await common.unit3d_distributor_ids(meta.get('distributor'))
         lt_name = await self.edit_name(meta)
-        if meta['anon'] == 0 and bool(distutils.util.strtobool(str(self.config['TRACKERS'][self.tracker].get('anon', "False")))) == False:
-            anon = 0
-        else:
+        if meta['anon'] != 0 or self.config['TRACKERS'][self.tracker].get('anon', False):
             anon = 1
+        else:
+            anon = 0
 
         if meta['bdinfo'] != None:
             mi_dump = None
