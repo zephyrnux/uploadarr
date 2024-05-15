c6e827fb08b0f8919b6ac66cc3b165cd3e8df20e
z-ink <159651875+z-ink@users.noreply.github.com>
2024-05-15 10:55:53 -0400

    Removes need for distutils

--- src/trackers/HP.py
+++ src/trackers/HP.py
@@ -2,7 +2,6 @@
 # import discord
 import asyncio
 import requests
-import distutils.util
 import os
 import platform
 
@@ -77,10 +76,10 @@ class HP():
         await common.unit3d_edit_desc(meta, self.tracker)
         region_id = await common.unit3d_region_ids(meta.get('region'))
         distributor_id = await common.unit3d_distributor_ids(meta.get('distributor'))
-        if meta['anon'] == 0 and bool(distutils.util.strtobool(str(self.config['TRACKERS'][self.tracker].get('anon', "False")))) == False:
-            anon = 0
-        else:
+        if meta['anon'] != 0 or self.config['TRACKERS'][self.tracker].get('anon', False):
             anon = 1
+        else:
+            anon = 0
 
         if meta['bdinfo'] != None:
             mi_dump = None
