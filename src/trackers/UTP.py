ba8605fd049580f0ed1efdb2c5deb0b737d3f1e6
z-ink <159651875+z-ink@users.noreply.github.com>
2024-05-15 12:06:04 -0400

    Support for Utopia + naming convention

---
+++ src/trackers/UTP.py
@@ -0,0 +1,295 @@
+# -*- coding: utf-8 -*-
+import asyncio
+import requests
+from src.trackers.COMMON import COMMON
+from src.console import console
+
+class UTP():
+    """
+    Edit for Tracker:
+        Edit BASE.torrent with announce and source
+        Check for duplicates
+        Set type/category IDs
+        Upload
+    """
+    def __init__(self, config):
+        self.config = config
+        self.tracker = 'UTP'
+        self.source_flag = 'UTOPIA'
+        self.search_url = 'https://utp.to/api/torrents/filter'
+        self.torrent_url = 'https://utp.to/api/torrents/'
+        self.upload_url = 'https://utp.to/api/torrents/upload'        
+        self.banned_groups = [""]
+        pass
+
+    async def upload(self, meta):
+        common = COMMON(config=self.config)
+        await common.edit_torrent(meta, self.tracker, self.source_flag)
+        await common.unit3d_edit_desc(meta, self.tracker, self.signature, comparison=True)
+        cat_id = await self.get_cat_id(meta['category'], meta.get('edition', ''))
+        type_id = await self.get_type_id(meta['type'])
+        resolution_id = await self.get_res_id(meta['resolution'])
+        region_id = await common.unit3d_region_ids(meta.get('region'))
+        distributor_id = await common.unit3d_distributor_ids(meta.get('distributor'))
+        if meta['anon'] != 0 or self.config['TRACKERS'][self.tracker].get('anon', False):
+            anon = 1
+        else:
+            anon = 0
+
+        if meta['bdinfo'] != None:
+            mi_dump = None
+            bd_dump = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/BD_SUMMARY_00.txt", 'r', encoding='utf-8').read()
+        else:
+            mi_dump = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/MEDIAINFO.txt", 'r', encoding='utf-8').read()
+            bd_dump = None
+        desc = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/[{self.tracker}DESCRIPTION.txt", 'r').read()
+        open_torrent = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/[{self.tracker}]{meta['clean_name']}.torrent", 'rb')
+        files = {'torrent': ("placeholder.torrent", open_torrent, "application/x-bittorrent")}
+        data = {
+            'name' : meta['name'],
+            'description' : desc,
+            'mediainfo' : mi_dump,
+            'bdinfo' : bd_dump,
+            'category_id' : cat_id,
+            'type_id' : type_id,
+            'resolution_id' : resolution_id,
+            'tmdb' : meta['tmdb'],
+            'imdb' : meta['imdb_id'].replace('tt', ''),
+            'tvdb' : meta['tvdb_id'],
+            'mal' : meta['mal_id'],
+            'igdb' : 0,
+            'anonymous' : anon,
+            'stream' : meta['stream'],
+            'sd' : meta['sd'],
+            'keywords' : meta['keywords'],
+            'personal_release' : int(meta.get('personalrelease', False)),
+            'internal' : 0,
+            'featured' : 0,
+            'free' : 0,
+            'doubleup' : 0,
+            'sticky' : 0
+        }
+        # Internal
+        if self.config['TRACKERS'][self.tracker].get('internal', False) == True:
+            if meta['tag'] != "" and (meta['tag'][1:] in self.config['TRACKERS'][self.tracker].get('internal_groups', [])):
+                data['internal'] = 1
+
+        if region_id != 0:
+            data['region_id'] = region_id
+        if distributor_id != 0:
+            data['distributor_id'] = distributor_id
+        if meta.get('category') == "TV":
+            data['season_number'] = meta.get('season_int', '0')
+            data['episode_number'] = meta.get('episode_int', '0')
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0'
+        }
+        params = {
+            'api_token': self.config['TRACKERS'][self.tracker]['api_key'].strip()
+        }
+
+        if meta['debug'] == False:
+            response = requests.post(url=self.upload_url, files=files, data=data, headers=headers, params=params)
+            try:
+                console.print(response.json())
+            except:
+                console.print("It may have uploaded, go check")
+
+                return
+        else:
+            console.print(f"[cyan]Request Data:")
+            console.print(data)
+        open_torrent.close()
+
+
+
+    async def get_cat_id(self, category_name, edition):
+        category_id = {
+            'MOVIE': '1',
+            'TV': '2',
+            'FANRES': '3'
+            }.get(category_name, '0')
+        if category_name == 'MOVIE' and 'FANRES' in edition:
+            category_id = '3'
+        return category_id
+
+    async def get_type_id(self, type):
+        type_id = {
+            'DISC': '1',
+            'REMUX': '2',
+            'WEBDL': '4',
+            'WEBRIP': '5',
+            'HDTV': '6',
+            'ENCODE': '3'
+            }.get(type, '0')
+        return type_id
+
+    async def get_res_id(self, resolution):
+        resolution_id = {
+            '4320p': '1',
+            '2160p': '2',
+            '1080p': '3',
+            '1080i': '4'
+            }.get(resolution, '1')
+        return resolution_id
+
+    async def get_name(self, meta):
+
+        basename = self.get_basename(meta)
+        type = meta.get('type', "")
+        title = meta.get('title',"")
+        alt_title = meta.get('aka', "")
+        year = meta.get('year', "")
+        resolution = meta.get('resolution', "")
+        if resolution == "OTHER":
+            resolution = ""
+        audio = meta.get('audio', "")
+        service = meta.get('service', "")
+        season = meta.get('season', "")
+        episode = meta.get('episode', "")
+        part = meta.get('part', "")
+        repack = meta.get('repack', "")
+        three_d = meta.get('3D', "")
+        tag = meta.get('tag', "")
+        if tag == "":
+            tag = "- NOGRP"
+        source = meta.get('source', "")
+        uhd = meta.get('uhd', "")
+        hdr = meta.get('hdr', "")
+        episode_title = meta.get('episode_title', '')
+        if meta.get('is_disc', "") == "BDMV": #Disk
+            video_codec = meta.get('video_codec', "")
+            region = meta.get('region', "")
+        elif meta.get('is_disc', "") == "DVD":
+            region = meta.get('region', "")
+            dvd_size = meta.get('dvd_size', "")
+        else:
+            video_codec = meta.get('video_codec', "")
+            video_encode = meta.get('video_encode', video_codec)
+        edition = meta.get('edition', "")
+
+        if meta['category'] == "TV":
+            try:
+                year = meta['year']
+                if not year: 
+                    raise ValueError("No TMDB Year Found..trying IMDB")
+            except (KeyError, ValueError):
+                try:
+                    year = meta['imdb_info']['year']
+                    if not year:
+                        raise ValueError("No IMDB Year Found..")
+                except (KeyError, ValueError):
+                    year = ""
+        if meta.get('no_season', False) == True:
+            season = ''
+        if meta.get('no_year', False) == True:
+            year = ''
+        if meta.get('no_aka', False) == True:
+            alt_title = ''
+        if meta['debug']:
+            console.log("[cyan]get_name cat/type")
+            console.log(f"CATEGORY: {meta['category']}")
+            console.log(f"TYPE: {meta['type']}")
+            console.log("[cyan]get_name meta:")
+            console.log(meta)
+
+        #YAY NAMING FUN
+        if meta['category'] == "MOVIE": #MOVIE SPECIFIC
+            if type == "DISC": #Disk
+                if meta['is_disc'] == 'BDMV':
+                    name = f"{title} {alt_title} {year} {three_d} {edition} {repack} {resolution} {region} {uhd} {source} {hdr} {video_codec} {audio}"
+                    potential_missing = ['edition', 'region', 'distributor']
+                elif meta['is_disc'] == 'DVD': 
+                    name = f"{title} {alt_title} {year} {edition} {repack} {source} {dvd_size} {audio}"
+                    potential_missing = ['edition', 'distributor']
+                elif meta['is_disc'] == 'HDDVD':
+                    name = f"{title} {alt_title} {year} {edition} {repack} {source} {audio}"
+                    potential_missing = ['edition', 'region', 'distributor']
+            elif type == "REMUX": #Remux
+                if source.upper() == 'BLURAY': 
+                    name = f"{title} {alt_title} {year} {three_d} {edition} {repack} {uhd} BDRemux {resolution} {hdr}" 
+                    potential_missing = ['edition', 'description']
+                if source.upper() == 'HDDVD':
+                    name = f"{title} {alt_title} {year} {edition} {repack} DVDRemux"
+                    potential_missing = ['edition', 'description']    
+                elif source in ("PAL DVD", "NTSC DVD", "DVD"): #DVD Remux
+                    name = f"{title} {alt_title} {year} {edition} {repack} {source} DVDRemux  {audio}" 
+                    potential_missing = ['edition', 'description']
+            elif type == "ENCODE": #Encode
+                name = f"{title} {alt_title} {year} {edition} {repack} BDRip {resolution} {hdr}"  
+                potential_missing = ['edition', 'description']
+            elif type == "WEBDL": #WEB-DL
+                name = f"{title} {alt_title} {year} {edition} {repack} {service} WEB-DL {resolution} {hdr}"
+                potential_missing = ['edition', 'service']
+            elif type == "WEBRIP": #WEBRip
+                name = f"{title} {alt_title} {year} {edition} {repack} {resolution} {service} WEBRip {audio} {hdr}"
+                potential_missing = ['edition', 'service']
+            elif type == "HDTV": #HDTV
+                name = f"{title} {alt_title} {year} {edition} {repack} {resolution} HDTV {audio} {video_encode}"
+                potential_missing = []
+        elif meta['category'] == "TV": #TV SPECIFIC
+            if type == "DISC": #Disk
+                if meta['is_disc'] == 'BDMV':
+                    name = f"{title} {year} {alt_title} {season}{episode} {three_d} {edition} {repack} {resolution} {region} {uhd} {source} {hdr} {video_codec} {audio}"
+                    potential_missing = ['edition', 'region', 'distributor']
+                if meta['is_disc'] == 'DVD':
+                    name = f"{title} {alt_title} {season}{episode}{three_d} {edition} {repack} {source} {dvd_size} {audio}"
+                    potential_missing = ['edition', 'distributor']
+                elif meta['is_disc'] == 'HDDVD':
+                    name = f"{title} {alt_title} {year} {edition} {repack} {source} {audio}"
+                    potential_missing = ['edition', 'region', 'distributor']
+            elif type == "REMUX": #Remux
+                if source.upper() == 'BLURAY':
+                    name = f"{title} {alt_title} {season}{episode} {year} {three_d} {edition} {repack} BDRemux {resolution} {hdr}" 
+                    potential_missing = ['edition', 'description']
+                elif source.upper() == 'HDDVD':
+                    name = f"{title} {alt_title} {season}{episode} {episode_title} {year} {edition} {repack} DVDRemux"
+                    potential_missing = ['edition', 'description']    
+                elif source in ("PAL DVD", "NTSC DVD"): #DVD Remux
+                    name = f"{title} {alt_title} {year} {edition} {repack} {source} DVDRemux {audio}" 
+                    potential_missing = ['edition', 'description']
+            elif type == "ENCODE": #Encode
+                name = f"{title} {year} {alt_title} {season}{episode} {episode_title} {edition} {repack} {resolution} BDRip {hdr} {video_encode}" 
+                potential_missing = ['edition', 'description']
+            elif type == "WEBDL": #WEB-DL
+                name = f"{title} {alt_title} {season}{episode} {episode_title} {year} {edition} {repack} {service} WEB-DL {resolution} {hdr}"
+                potential_missing = ['edition', 'service']
+            elif type == "WEBRIP": #WEBRip
+                name = f"{title} {alt_title} {season}{episode} {episode_title} {year} {edition} {repack} {service} WEBRip {resolution} {hdr}"
+                potential_missing = ['edition', 'service']
+            elif type == "HDTV": #HDTV
+                name = f"{title} {year} {alt_title} {season}{episode} {episode_title} {edition} {repack} {resolution} HDTV {audio} {video_encode}"
+                potential_missing = []
+
+        
+        return ' '.join(name.split())
+
+
+    async def search_existing(self, meta):
+        dupes = []
+        console.print("[yellow]Searching for existing torrents on site...")
+        params = {
+            'api_token' : self.config['TRACKERS'][self.tracker]['api_key'].strip(),
+            'tmdbId' : meta['tmdb'],
+            'categories[]' : await self.get_cat_id(meta['category'], meta.get('edition', '')),
+            'types[]' : await self.get_type_id(meta['type']),
+            'resolutions[]' : await self.get_res_id(meta['resolution']),
+            'name' : ""
+        }
+        if meta['category'] == 'TV':
+            params['name'] = params['name'] + f" {meta.get('season', '')}{meta.get('episode', '')}"
+        if meta.get('edition', "") != "":
+            params['name'] = params['name'] + f" {meta['edition']}"
+        try:
+            response = requests.get(url=self.search_url, params=params)
+            response = response.json()
+            for each in response['data']:
+                result = [each][0]['attributes']['name']
+                # difference = SequenceMatcher(None, meta['clean_name'], result).ratio()
+                # if difference >= 0.05:
+                dupes.append(result)
+        except:
+            console.print('[bold red]Unable to search for existing torrents on site. Either the site is down or your API key is incorrect')
+            await asyncio.sleep(5)
+
+        return dupes
