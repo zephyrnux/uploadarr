0f8c37a4f22a2fb0ad414bb064bae870afc0dee5
z-ink <159651875+z-ink@users.noreply.github.com>
2024-06-03 12:15:38 -0400

    Anime season interger fix - thanks Snackington!

--- src/trackers/LDU.py
+++ src/trackers/LDU.py
@@ -185,8 +185,8 @@ class LDU():
         if distributor_id != 0:
             data['distributor_id'] = distributor_id
         if meta.get('category') == "TV":
-            data['season_number'] = meta.get('season_int', '0')
-            data['episode_number'] = meta.get('episode_int', '0')
+            data['season_number'] = int(meta.get('season_int', '0'))
+            data['episode_number'] = int(meta.get('episode_int', '0'))
         headers = {
             'User-Agent': f'Uploadrr / v1.0 ({platform.system()} {platform.release()})'
         }
