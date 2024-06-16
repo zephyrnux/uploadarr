3330e9df4e65b5870bdd74937870acd71da8db75
CvT <159651875+z-ink@users.noreply.github.com>
2024-06-15 20:04:32 -0400

    updated BAN list

--- src/trackers/ULCX.py
+++ src/trackers/ULCX.py
@@ -18,7 +18,7 @@ class ULCX():
         self.source_flag = 'ULCX'
         self.upload_url = 'https://upload.cx/api/torrents/upload'
         self.search_url = 'https://upload.cx/api/torrents/filter'
-        self.banned_groups = ['Tigole', 'x0r', 'Judas', 'SPDVD', 'MeGusta', 'YIFY', 'SWTYBLZ', 'TAoE', 'TSP', 'TSPxL', 'LAMA', '4K4U', 'ION10', 'Will1869', 'TGx', 'Sicario', 'QxR', 'Hi10', 'EMBER', 'FGT', 'AROMA', 'd3g', 'nikt0', 'Grym', 'RARBG', 'iVy', 'NuBz', 'NAHOM']
+        self.banned_groups = ['Tigole', 'x0r', 'Judas', 'SPDVD', 'MeGusta', 'YIFY', 'SWTYBLZ', 'TAoE', 'TSP', 'TSPxL', 'LAMA', '4K4U', 'ION10', 'Will1869', 'TGx', 'Sicario', 'QxR', 'Hi10', 'EMBER', 'FGT', 'AROMA', 'd3g', 'nikt0', 'Grym', 'RARBG', 'iVy', 'NuBz', 'NAHOM', 'EDGE2020', 'FnP']
         pass
     
     async def get_cat_id(self, category_name):
