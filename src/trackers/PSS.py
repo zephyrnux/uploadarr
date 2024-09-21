b24a359687216ae1ff4fe86a5533dc0024cdc31e
CvT <CvT@noreply.codeberg.org>
2024-09-21 11:18:52 -0400

    PSS fix

--- src/trackers/PSS.py
+++ src/trackers/PSS.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 import asyncio
 import requests
-from difflib import SequenceMatcher
 import json
 import os
 import platform
 
 from src.trackers.COMMON import COMMON
-from src.console import console 
+from src.console import console
+
 
 class PSS():
 
@@ -17,24 +17,65 @@ class PSS():
         self.source_flag = 'PSS'
         self.upload_url = 'https://privatesilverscreen.cc/api/torrents/upload'
         self.search_url = 'https://privatesilverscreen.cc/api/torrents/filter'
+        self.signature = None
         self.banned_groups = [ "Will1869", "x0r", "rarbg", "NeXus", "core", "ZMNT", "d3g", "msd", "MeGusta", "FGT", "nikt0", "STUTTERSHIT", "$andra", "AROMA", "aXXo", "BRrip", "CM8", "CrEwSaDe",
     "DNL", "FaNGDiNG0", "FRDS", "HD2DVD", "HDTime", "iPlanet", "KiNGDOM", "Leffe", "mHD", "nHD", "nSD", "NhaNc3", "PRODJi", "RDN", "SANTi", "ViSION", "WAF", "YIFY", "YTS",
     "FROZEN", "UTR", "Grym", "GrymLegacy", "C4K", "tigole", "ProRes", "MezRips", "GalaxyRG", "RCDiVX", "AR", "LycanHD", "TSP", "TSPxL"]
 
         pass
     
+    async def get_cat_id(self, category_name):
+        category_id = {
+            'MOVIE': '1', 
+            'TV': '2', 
+            }.get(category_name, '0')
+        return category_id
+
+    async def get_type_id(self, type):
+        type_id = {
+            'DISC': '1', 
+            'REMUX': '2',
+            'WEBDL': '4', 
+            'WEBRIP': '5', 
+            'HDTV': '6',
+            'ENCODE': '3'
+            }.get(type, '0')
+        return type_id
+
+    async def get_res_id(self, resolution):
+        resolution_id = {
+            '8640p':'10', 
+            '4320p': '1', 
+            '2160p': '2', 
+            '1440p' : '3',
+            '1080p': '3',
+            '1080i':'4', 
+            '720p': '5',  
+            '576p': '6', 
+            '576i': '7',
+            '480p': '8', 
+            '480i': '9'
+            }.get(resolution, '10')
+        return resolution_id
+
+    ###############################################################
+    ######   STOP HERE UNLESS EXTRA MODIFICATION IS NEEDED   ######
+    ###############################################################
+
     async def upload(self, meta):
         common = COMMON(config=self.config)
         await common.edit_torrent(meta, self.tracker, self.source_flag)
-        await common.unit3d_edit_desc(meta, self.tracker)
         cat_id = await self.get_cat_id(meta['category'])
-        type_id = await self.get_type_id(meta['type'], meta.get('tv_pack', 0), meta.get('sd', 0), meta.get('category', ""))
+        type_id = await self.get_type_id(meta['type'])
         resolution_id = await self.get_res_id(meta['resolution'])
-        stc_name = await self.edit_name(meta)
+        await common.unit3d_edit_desc(meta, self.tracker)
+        region_id = await common.unit3d_region_ids(meta.get('region'))
+        distributor_id = await common.unit3d_distributor_ids(meta.get('distributor'))
         if meta['anon'] != 0 or self.config['TRACKERS'][self.tracker].get('anon', False):
             anon = 1
         else:
             anon = 0
+
         if meta['bdinfo'] != None:
             mi_dump = None
             bd_dump = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/BD_SUMMARY_00.txt", 'r', encoding='utf-8').read()
@@ -45,7 +86,7 @@ class PSS():
         open_torrent = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/[{self.tracker}]{meta['clean_name']}.torrent", 'rb')
         files = {'torrent': open_torrent}
         data = {
-            'name' : stc_name,
+            'name' : meta['name'],
             'description' : desc,
             'mediainfo' : mi_dump,
             'bdinfo' : bd_dump, 
@@ -73,14 +114,18 @@ class PSS():
             if meta['tag'] != "" and (meta['tag'][1:] in self.config['TRACKERS'][self.tracker].get('internal_groups', [])):
                 data['internal'] = 1
                 
+        if region_id != 0:
+            data['region_id'] = region_id
+        if distributor_id != 0:
+            data['distributor_id'] = distributor_id
         if meta.get('category') == "TV":
             data['season_number'] = meta.get('season_int', '0')
             data['episode_number'] = meta.get('episode_int', '0')
         headers = {
-            'User-Agent': f'Uploadrr ({platform.system()} {platform.release()})'
+            'User-Agent': f'Uploadrr / v1.0 ({platform.system()} {platform.release()})'
         }
         params = {
-            'api_token': self.config['TRACKERS'][self.tracker]['api_key'].strip()
+            'api_token' : self.config['TRACKERS'][self.tracker]['api_key'].strip()
         }
         
         if not meta['debug']:
@@ -120,63 +165,6 @@ class PSS():
 
 
 
-    async def edit_name(self, meta):
-        stc_name = meta.get('name')
-        return stc_name
-
-    async def get_cat_id(self, category_name):
-        category_id = {
-            'MOVIE': '1', 
-            'TV': '2', 
-            }.get(category_name, '0')
-        return category_id
-
-    async def get_type_id(self, type, tv_pack, sd, category):
-        type_id = {
-            'DISC': '1', 
-            'REMUX': '2',
-            'WEBDL': '4', 
-            'WEBRIP': '5', 
-            'HDTV': '6',
-            'ENCODE': '3'
-            }.get(type, '0')
-        if tv_pack == 1:
-            if sd == 1:
-                # Season SD
-                type_id = '14'
-                if type == "ENCODE":
-                    type_id = '18'
-            if sd == 0:
-                # Season HD
-                type_id = '13'
-                if type == "ENCODE":
-                    type_id = '18'
-        if type == "DISC" and category == "TV":
-            if sd == 1:
-                # SD-RETAIL
-                type_id = '17'
-            if sd == 0:
-                # HD-RETAIL
-                type_id = '18'
-        return type_id
-
-    async def get_res_id(self, resolution):
-        resolution_id = {
-            '8640p':'10', 
-            '4320p': '1', 
-            '2160p': '2', 
-            '1440p' : '3',
-            '1080p': '3',
-            '1080i':'4', 
-            '720p': '5',  
-            '576p': '6', 
-            '576i': '7',
-            '480p': '8', 
-            '480i': '9'
-            }.get(resolution, '10')
-        return resolution_id
-
-
     async def search_existing(self, meta):
         dupes = {}
         console.print("[yellow]Searching for existing torrents on site...")
@@ -184,14 +172,12 @@ class PSS():
             'api_token' : self.config['TRACKERS'][self.tracker]['api_key'].strip(),
             'tmdbId' : meta['tmdb'],
             'categories[]' : await self.get_cat_id(meta['category']),
-            'types[]' : await self.get_type_id(meta['type'], meta.get('tv_pack', 0), meta.get('sd', 0), meta.get('category', "")),
+            'types[]' : await self.get_type_id(meta['type']),
             'resolutions[]' : await self.get_res_id(meta['resolution']),
             'name' : ""
         }
-        if meta['category'] == 'TV':
-            params['name'] = f"{meta.get('season', '')}{meta.get('episode', '')}"
         if meta.get('edition', "") != "":
-            params['name'] + meta['edition']
+            params['name'] = params['name'] + f" {meta['edition']}"
         try:
             response = requests.get(url=self.search_url, params=params)
             response = response.json()
@@ -199,7 +185,7 @@ class PSS():
                 result = each['attributes']['name']
                 size = each['attributes']['size']
                 dupes[result] = size
-        except:
+        except Exception:
             console.print('[bold red]Unable to search for existing torrents on site. Either the site is down or your API key is incorrect')
             await asyncio.sleep(5)
 
